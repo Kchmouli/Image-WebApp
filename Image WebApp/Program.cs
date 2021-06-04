329618013d1e595e3bb336ed9be387e0e681fb06
kchmouli <Chandramouli.kantipudi@gmail.com>
2021-06-03 20:38:26 -0500

    Update to db

--- Image WebApp/Program.cs
+++ Image WebApp/Program.cs
@@ -17,9 +17,23 @@ namespace Image_WebApp
         {
             var host = CreateHostBuilder(args).Build();
 
-            CreateDbIfNotExists(host);
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ImageContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
 
             host.Run();
+
         }
 
         private static void CreateDbIfNotExists(IHost host)
