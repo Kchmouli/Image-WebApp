6d909c16bfa70a92e6dba3263c2c2b75970c926f
kchmouli <Chandramouli.kantipudi@gmail.com>
2021-06-03 20:54:42 -0500

    Db

--- Image WebApp/Program.cs
+++ Image WebApp/Program.cs
@@ -15,22 +15,10 @@ namespace Image_WebApp
     {
         public static void Main(string[] args)
         {
+            
             var host = CreateHostBuilder(args).Build();
 
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<ImageContext>();
-                    DbInitializer.Initialize(context);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while seeding the database.");
-                }
-            }
+            CreateDbIfNotExists(host);
 
             host.Run();
 
